c97e9310d39c02dbed67af5db1e62773a66542cd
xdivayze <ozangencay123@gmail.com>
2025-05-18 20:07:39 +0300

    added dropdown menu, follow new branch

--- frontend/src/Pages/LogWorkout/Components/MainPanel/MainPanel.tsx
+++ frontend/src/Pages/LogWorkout/Components/MainPanel/MainPanel.tsx
@@ -16,76 +16,83 @@ export default function MainPanel() {
   } = useContext(MainPanelRefContext) as MainPanelRefContextType;
   const [partialSums, setPartialSums] = useState<ReactNode[]>([]);
   return (
-      <div className=" overflow-y-auto h-full w-full rounded-3xl bg-gray-ogg-1 p-5 flex items-center flex-col shadow-2xl">
-        <div className="min-h-14 w-full mb-4">
+    <div className=" overflow-y-auto h-full w-full rounded-3xl bg-gray-ogg-1 p-5 flex items-center flex-col shadow-2xl">
+      <div className="min-h-14 w-full mb-4">
+        <MiniPanel
+          placeholderText="Choose Exercise"
+          ref={exerciseChooseDivRef}
+          contentEditable={false}
+          color="bg-gray-ogg-2"
+          dropdownFeatures={{
+            items: ["hi", "george"],
+            onSelect(item) {
+              console.log(item);
+            },
+          }}
+        />
+      </div>
+      <div className="flex-row justify-between items-center pl-1 flex w-full font-inter font-light text-2xl mb-4 ">
+        <div> Current Set</div>
+        <div className="h-11 w-1/6">
           <MiniPanel
-            placeholderText="Choose Exercise"
-            ref={exerciseChooseDivRef}
             color="bg-gray-ogg-2"
-          />
-        </div>
-        <div className="flex-row justify-between items-center pl-1 flex w-full font-inter font-light text-2xl mb-4 ">
-          <div> Current Set</div>
-          <div className="h-11 w-1/6">
-            <MiniPanel
-              color="bg-gray-ogg-2"
-              ref={setNumberDivRef}
-              placeholderText="0"
-            />
-          </div>
-        </div>
-        <div className="flex-row flex justify-between w-full font-inter font-light text-2xl mb-4">
-          <div
-            onClick={(e) => {
-              e.preventDefault();
-              const psum = <GeneratePartialSummary />;
-              setPartialSums((prev) => [...prev, psum]);
-            }}
-            className="min-h-11 w-1/3 text-white cursor-pointer"
-          >
-            <MiniPanel
-              contentEditable={false}
-              placeholderText="Add"
-              color="bg-blue-ogg-0"
-            />
-          </div>
-          <div className="min-h-11 w-1/6">
-            <MiniPanel
-              ref={repWeightRef}
-              placeholderText="50"
-              color="bg-gray-ogg-2"
-            />
-          </div>
-          <div className="min-h-11 w-1/6  ">
-            <MiniPanel
-              ref={weightUnitRef}
-              placeholderText="kg"
-              color="bg-gray-ogg-2"
-            />
-          </div>
-          <div className="min-h-11 w-1/6">
-            <MiniPanel
-              ref={repCountRef}
-              placeholderText="12"
-              color="bg-gray-ogg-2"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col w-full mt-7">
-          {partialSums.map((v, i) => (
-            <div key={i} className="h-[70px] w-full mb-3">
-              {" "}
-              {v}{" "}
-            </div>
-          ))}
-        </div>
-        <div className="min-h-15 w-full mb-4 sticky text-white cursor-pointer bottom-0">
-          <MiniPanel
-            color="bg-blue-ogg-0 "
-            contentEditable={false}
-            placeholderText="Finish"
+            ref={setNumberDivRef}
+            placeholderText="0"
           />
         </div>
       </div>
+      <div className="flex-row flex justify-between w-full font-inter font-light text-2xl mb-4">
+        <div
+          onClick={(e) => {
+            e.preventDefault();
+            const psum = <GeneratePartialSummary />;
+            setPartialSums((prev) => [...prev, psum]);
+          }}
+          className="min-h-11 w-1/3 text-white cursor-pointer"
+        >
+          <MiniPanel
+            contentEditable={false}
+            placeholderText="Add"
+            color="bg-blue-ogg-0"
+          />
+        </div>
+        <div className="min-h-11 w-1/6">
+          <MiniPanel
+            ref={repWeightRef}
+            placeholderText="50"
+            color="bg-gray-ogg-2"
+          />
+        </div>
+        <div className="min-h-11 w-1/6  ">
+          <MiniPanel
+            ref={weightUnitRef}
+            placeholderText="kg"
+            color="bg-gray-ogg-2"
+          />
+        </div>
+        <div className="min-h-11 w-1/6">
+          <MiniPanel
+            ref={repCountRef}
+            placeholderText="12"
+            color="bg-gray-ogg-2"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col w-full mt-7">
+        {partialSums.map((v, i) => (
+          <div key={i} className="h-[70px] w-full mb-3">
+            {" "}
+            {v}{" "}
+          </div>
+        ))}
+      </div>
+      <div className="min-h-15 w-full mb-4 sticky text-white cursor-pointer bottom-0">
+        <MiniPanel
+          color="bg-blue-ogg-0 "
+          contentEditable={false}
+          placeholderText="Finish"
+        />
+      </div>
+    </div>
   );
 }
--- frontend/src/Pages/LogWorkout/Components/MainPanel/MiniPanel.tsx
+++ frontend/src/Pages/LogWorkout/Components/MainPanel/MiniPanel.tsx
@@ -1,4 +1,9 @@
-import type { RefObject } from "react";
+import { useState, type RefObject } from "react";
+
+interface IDropdownFeatures {
+  items: string[];
+  onSelect: (item: string) => void;
+}
 
 export default function MiniPanel({
   color,
@@ -6,25 +11,56 @@ export default function MiniPanel({
   ref,
   contentEditable = true,
   numeric = false,
+  dropdownFeatures,
 }: {
   color: string;
   placeholderText: string;
   ref?: RefObject<HTMLDivElement>;
   contentEditable?: boolean;
   numeric?: boolean;
+  dropdownFeatures?: IDropdownFeatures;
 }) {
+  const [selected, setSelected] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  let count = 0;
   return (
-    <div
-      className={`h-full w-full ${color} rounded-2xl font-inter font-light px-2 pb-1 text-2xl items-center justify-center flex`}
-      contentEditable={contentEditable}
-      suppressContentEditableWarning
-      ref={ref}
-      inputMode={numeric ? "numeric" : "text"}
-      
-    >
-      {" "}
-      {placeholderText}{" "}
+    <div className="h-full w-full relative inline-block">
+      <div
+        className={`h-full w-full ${color} rounded-2xl font-inter font-light px-2 pb-1 text-2xl items-center justify-center flex`}
+        contentEditable={contentEditable}
+        suppressContentEditableWarning
+        ref={ref}
+        inputMode={numeric ? "numeric" : "text"}
+        onClick={(e) => {
+          if (dropdownFeatures !== undefined) {
+            e.preventDefault();
+            setIsOpen(!isOpen);
+          }
+        }}
+      >
+        {selected !== "" ? selected : placeholderText}
+      </div>
+      {isOpen && (
+        <div className="w-full absolute z-10 rounded-xl shadow-lg font-inter font-light bg-gray-ogg-2 border border-gray-300  ">
+          <div className=" w-full flex flex-col ">
+            {dropdownFeatures?.items.map((item) => {
+              count += 1
+              return(
+              <div
+              className={`${count % 2 == 0 ? "bg-gray-ogg-1" : "bg-gray-ogg-2"} `}
+                key={item}
+                onClick={() => {
+                  setSelected(item);
+                  setIsOpen(false);
+                  dropdownFeatures.onSelect(item);
+                }}
+              >
+                <span className="p-1">{item}</span>
+              </div>
+            )})}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
- 
