a7b0e3dbaf21bf5b407dc4070786319f06b71955
xdivayze <ozangencay123@gmail.com>
2025-05-31 09:20:49 +0300

    added success page and na vigation to log workout page

--- frontend/src/Pages/Secure/Login/Components/MainPanel.tsx
+++ frontend/src/Pages/Secure/Login/Components/MainPanel.tsx
@@ -1,12 +1,15 @@
-import { useContext, useEffect, useRef, type RefObject } from "react";
+import { useContext, useRef, useState, type RefObject } from "react";
 import MiniPanel from "../../LogWorkout/Components/MainPanel/MiniPanel";
 import { DoLogin } from "./Service";
 import { authContext } from "../../SecurityContext";
+import { useNavigate } from "react-router-dom";
 
 export default function MainPanel() {
   const idRef = useRef<HTMLDivElement>(null) as RefObject<HTMLDivElement>;
   const passwordRef = useRef<HTMLDivElement>(null) as RefObject<HTMLDivElement>;
   const context = useContext(authContext);
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const navigate = useNavigate()
   return (
     <div
       className=" overflow-y-auto h-full w-full rounded-3xl
@@ -18,45 +21,61 @@ export default function MainPanel() {
       >
         Login
       </div>
-      <div className="w-full h-15 mt-[24px]">
-        <MiniPanel
-          ref={idRef}
-          placeholderText="enter your identifier"
-          color="bg-gray-ogg-2"
-          contentEditable={true}
-        />
-      </div>
-      <div className="w-full h-15 mt-4">
-        <MiniPanel
-          ref={passwordRef}
-          placeholderText="enter your password"
-          color="bg-gray-ogg-2"
-          contentEditable={true}
-        />
-      </div>
-      <div
-        className="w-full h-15 mt-[50px]"
-        onClick={() => {
-          if (!context) {
-            throw "function is not within a context provider";
-          }
-          DoLogin(idRef.current.innerText, passwordRef.current.innerText)
-            .catch((e: Error) => {
-              console.error(e); //TODO add actual error handling here
-              return;
-            })
-            .then((val) => {
-              if (!val) throw "user auth code not returned";
-              context.login(val.authCode, val.expiresAt);
-            });
-        }}
-      >
-        <MiniPanel
-          placeholderText="Continue"
-          color="bg-blue-ogg-0 text-white cursor-pointer shadow-xl"
-          contentEditable={false}
-        />
-      </div>
+
+      {!loginSuccess && (
+        <>
+          <div className="w-full h-15 mt-[24px]">
+            <MiniPanel
+              ref={idRef}
+              placeholderText="enter your identifier"
+              color="bg-gray-ogg-2"
+              contentEditable={true}
+            />
+          </div>
+          <div className="w-full h-15 mt-4">
+            <MiniPanel
+              ref={passwordRef}
+              placeholderText="enter your password"
+              color="bg-gray-ogg-2"
+              contentEditable={true}
+            />
+          </div>
+          <div
+            className="w-full h-15 mt-[50px]"
+            onClick={() => {
+              if (!context) {
+                throw "function is not within a context provider";
+              }
+              DoLogin(idRef.current.innerText, passwordRef.current.innerText)
+                .catch((e: Error) => {
+                  console.error(e); //TODO add actual error handling here
+                  return;
+                })
+                .then((val) => {
+                  if (!val) throw "user auth code not returned";
+                  context.login(val.authCode, val.expiresAt);
+                  setLoginSuccess(true);
+                  setTimeout(() => {
+                    navigate("/log-workout")
+                  }, 3000)
+                });
+            }}
+          >
+            <MiniPanel
+              placeholderText="Continue"
+              color="bg-blue-ogg-0 text-white cursor-pointer shadow-xl"
+              contentEditable={false}
+            />
+          </div>
+        </>
+      )}
+      {loginSuccess && (
+        <div className="w-full h-15 mt-[24px] text-center text-xl font-inter ">
+          Login Success,{" "}
+          <span className="text-blue-ogg-0 cursor-pointer">redirecting</span>{" "}
+          now...
+        </div>
+      )}
     </div>
   );
 }
