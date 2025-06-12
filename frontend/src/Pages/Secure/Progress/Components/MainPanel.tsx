08a754292ca5dd39bbbddb2c63c925f060f97568
xdivayze <ozangencay123@gmail.com>
2025-06-12 11:34:23 +0300

    feat: implement DatePanel component for date selection in MainPanel

--- frontend/src/Pages/Secure/Progress/Components/MainPanel.tsx
+++ frontend/src/Pages/Secure/Progress/Components/MainPanel.tsx
@@ -1,19 +1,39 @@
-import { useRef, type RefObject } from "react";
+import { useRef, useState, type RefObject } from "react";
 import ChooseExerciseMenu from "../../LogWorkout/Components/MainPanel/ChooseExerciseMenu/ChooseExerciseMenu";
+
+import DatePanel from "./DatePanel";
 
 export default function MainPanel() {
   const exerciseChooseDivRef = useRef<HTMLDivElement | null>(
     null
   ) as RefObject<HTMLDivElement>;
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
   return (
     <div
       className=" overflow-y-auto h-full w-full rounded-3xl
-     bg-gray-ogg-1 p-5 flex items-center flex-col shadow-2xl"
+     bg-gray-ogg-1 p-5 flex items-center flex-col "
     >
-      <div className=" min-h-14 w-full mb-4 cursor-pointer">
+      <div className=" min-h-14 w-full mb-4 cursor-pointer ">
         <ChooseExerciseMenu
           externalExerciseChooseDivRef={exerciseChooseDivRef}
         />
+      </div>
+      <div className="min-h-14 w-full mb-4  flex flex-row justify-between items-center">
+        <div className="w-1/2 mr-1 h-full ">
+          <DatePanel
+            nWeeksBack={6}
+            text="Start Date"
+            onChange={(d) => setStartDate(d)}
+          />
+        </div>
+        <div className="w-1/2 ml-1 h-full ">
+          <DatePanel
+            nWeeksBack={6}
+            text="End Date"
+            onChange={(d) => setEndDate(d)}
+          />
+        </div>
       </div>
     </div>
   );
