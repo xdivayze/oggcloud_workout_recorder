75e255eef1a3b901bffad535c914323d9df0fadd
xdivayze <ozangencay123@gmail.com>
2025-06-14 14:50:35 +0300

    feat: implement auth code expiration handling and improve login response structure add database cleanup

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"backend/src/db"
 	"backend/src/routes"
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -24,18 +29,43 @@ func main() {
 		log.Fatalf("error occurred while connecting to db: \n\t%v", err)
 	}
 
-	defer func() {
-		defer db.DB.Migrator().DropTable(db.TABLES...)
-		log.Println("Database connection closed")
-	}()
-
 	r := gin.Default()
 	routes.RegisterRoutes(r)
 
-	if err := r.Run(fmt.Sprintf(":%d", PORT)); err != nil {
-		log.Fatalf("error occurred while running the server: \n\t%v", err)
+	r.Static("/assets", "./frontend/assets")
+	r.StaticFile("/", "./frontend/index.html")
+	r.NoRoute(func(c *gin.Context) {
+		c.File("./frontend/index.html")
+	})
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGKILL, syscall.SIGTERM)
+	defer stop()
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", PORT),
+		Handler: r,
 	}
 
-	log.Printf("Server is running on port %d", PORT)
+	go func() {
+		log.Printf("Starting server on port %d...\n", PORT)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v\n", err)
+		}
+	}()
+
+	<-ctx.Done() // Wait for shutdown signal
+	log.Println("Received shutdown signal, shutting down server...")
+
+	if err := srv.Shutdown(context.Background()); err != nil { // Gracefully shutdown the server
+		log.Fatalf("Error shutting down server: %v\n", err)
+	}
+
+	log.Println("Server shut down gracefully")
+	if os.Getenv("DEV") == "true" {
+		// Drop tables only in development mode
+		log.Println("Dropping tables...")
+		db.DB.Migrator().DropTable(db.TABLES...)
+	}
+	log.Println("Database connection closed")
 
 }
